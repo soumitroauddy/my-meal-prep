01ac851c86bd730e3ac1af9fc7b236ad867ae772
Soumitro Auddy <skunkworks@Soumitros-MacBook-Pro.local>
2025-09-19 18:08:12 +0530

    Scaffolding

---
+++ src/main/java/com/magistrati/mymealprepper/context/UserContext.java
@@ -0,0 +1,12 @@
+package com.magistrati.mymealprepper.context;
+
+import java.util.List;
+
+public class UserContext {
+    private String userId;
+    private List<String> allergies;
+    private List<String> dietaryPreferences;
+    private List<String> likedIngredients;
+    private List<String> dislikedIngredients;
+    
+}
